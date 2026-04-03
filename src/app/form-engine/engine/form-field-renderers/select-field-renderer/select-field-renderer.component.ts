18051ff323c15d584e144f90ef3b887eb981dcc2
Marcin Spasinski <marcin7789@gmail.com>
2026-04-03 17:58:24 +0200

    Add select form field renderer

---
+++ src/app/form-engine/engine/form-field-renderers/select-field-renderer/select-field-renderer.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FieldRenderer,
+  SelectControlSchema,
+} from '../../../schema/form-control.model';
+
+@Component({
+  selector: 'app-select-renderer',
+  template: `
+    <select [formControl]="control">
+      @if (controlSchema.placeholder || controlSchema.label) {
+        <option [ngValue]="null" disabled>
+          {{ controlSchema.placeholder || controlSchema.label }}
+        </option>
+      }
+
+      @for (option of controlSchema.options; track option.value) {
+        <option [value]="option.value">
+          {{ option.label }}
+        </option>
+      }
+    </select>
+  `,
+  standalone: true,
+  imports: [ReactiveFormsModule],
+})
+export class SelectRendererComponent implements FieldRenderer<SelectControlSchema> {
+  @Input() control!: FormControl;
+  @Input() controlSchema!: SelectControlSchema;
+}
