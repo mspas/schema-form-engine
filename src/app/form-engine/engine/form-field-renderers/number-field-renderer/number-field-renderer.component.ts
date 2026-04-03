3c7bf30dd6b4bb26fcd24c3a9237a02df1406b2f
Marcin Spasinski <marcin7789@gmail.com>
2026-04-03 17:57:12 +0200

    Add numerical form field renderer

---
+++ src/app/form-engine/engine/form-field-renderers/number-field-renderer/number-field-renderer.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FieldRenderer,
+  NumberControlSchema,
+} from '../../../schema/form-control.model';
+
+@Component({
+  selector: 'app-number-renderer',
+  template: `
+    <input
+      type="number"
+      [formControl]="control"
+      [attr.min]="controlSchema.min"
+      [attr.max]="controlSchema.max"
+      [placeholder]="controlSchema.placeholder || controlSchema.label" />
+  `,
+  standalone: true,
+  imports: [ReactiveFormsModule],
+})
+export class NumberRendererComponent implements FieldRenderer<NumberControlSchema> {
+  @Input() control!: FormControl;
+  @Input() controlSchema!: NumberControlSchema;
+}
