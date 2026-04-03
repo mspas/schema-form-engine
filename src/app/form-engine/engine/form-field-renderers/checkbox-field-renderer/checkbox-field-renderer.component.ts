be057c16ff05ae159af4906cfe98004fb27da425
Marcin Spasinski <marcin7789@gmail.com>
2026-04-03 17:58:13 +0200

    Add checkbox form field renderer

---
+++ src/app/form-engine/engine/form-field-renderers/checkbox-field-renderer/checkbox-field-renderer.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  FieldRenderer,
+  CheckboxControlSchema,
+} from '../../../schema/form-control.model';
+
+@Component({
+  selector: 'app-checkbox-renderer',
+  template: `
+    <input type="checkbox" [formControl]="control" />
+  `,
+  standalone: true,
+  imports: [ReactiveFormsModule],
+})
+export class CheckboxRendererComponent implements FieldRenderer<CheckboxControlSchema> {
+  @Input() control!: FormControl;
+  @Input() controlSchema!: CheckboxControlSchema;
+}
